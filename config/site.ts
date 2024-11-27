b7cc07353be6d243f24b153c19c59c2ca81f2678
Risa Goto <omusubi5656dup@gmail.com>
2024-11-27 12:47:34 +0900

    リンク先　Git,X追加

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+    name: "Portfolio",
+    description: "",
+    url:"http://localhost:3001/",
+    ogImage:"",
+    links: {
+        x:"https://x.com/sarutahiko510",
+        github:"https://github.com/Risa2024",
+    },
+};
